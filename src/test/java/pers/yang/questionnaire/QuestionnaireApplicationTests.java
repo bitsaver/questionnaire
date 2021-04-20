b3425fd726c26ff682ecb8985cffd35495d86e76
bitsaver <1198790425@qq.com>
2021-04-20 14:19:01 +0800

    Initial commit

---
+++ src/test/java/pers/yang/questionnaire/QuestionnaireApplicationTests.java
@@ -0,0 +1,14 @@
+package pers.yang.questionnaire;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionnaireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
